8ab1ba64a4b51f27a69dd740f1af229b6476ccec
Batuhan <118824668+sunrise1907@users.noreply.github.com>
2024-08-08 01:23:44 +0300

    Proje dosyası ekle.

---
+++ bmp32/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace bmp32
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.topPanel = new Guna.UI2.WinForms.Guna2Panel();
+            this.closeBtn = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.openMusic = new Guna.UI2.WinForms.Guna2Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.minimizeBtn = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.topPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // topPanel
+            // 
+            this.topPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(84)))), ((int)(((byte)(101)))), ((int)(((byte)(150)))));
+            this.topPanel.Controls.Add(this.guna2HtmlLabel1);
+            this.topPanel.Controls.Add(this.minimizeBtn);
+            this.topPanel.Controls.Add(this.closeBtn);
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(364, 28);
+            this.topPanel.TabIndex = 0;
+            this.topPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.topPanel_MouseDown);
+            this.topPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.topPanel_MouseMove);
+            this.topPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.topPanel_MouseUp);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.closeBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.closeBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.closeBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.closeBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.closeBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.closeBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.closeBtn.ForeColor = System.Drawing.Color.White;
+            this.closeBtn.Location = new System.Drawing.Point(348, 3);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.closeBtn.Size = new System.Drawing.Size(16, 16);
+            this.closeBtn.TabIndex = 1;
+            this.closeBtn.Text = "x";
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 22);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(364, 215);
+            this.axWindowsMediaPlayer1.TabIndex = 1;
+            // 
+            // openMusic
+            // 
+            this.openMusic.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.openMusic.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.openMusic.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.openMusic.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.openMusic.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.openMusic.FillColor = System.Drawing.Color.LightSteelBlue;
+            this.openMusic.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.openMusic.ForeColor = System.Drawing.Color.Black;
+            this.openMusic.Location = new System.Drawing.Point(329, 208);
+            this.openMusic.Name = "openMusic";
+            this.openMusic.Size = new System.Drawing.Size(34, 29);
+            this.openMusic.TabIndex = 2;
+            this.openMusic.Text = "+";
+            this.openMusic.Click += new System.EventHandler(this.openMusic_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // minimizeBtn
+            // 
+            this.minimizeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.minimizeBtn.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.minimizeBtn.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.minimizeBtn.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.minimizeBtn.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.minimizeBtn.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.minimizeBtn.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.minimizeBtn.ForeColor = System.Drawing.Color.White;
+            this.minimizeBtn.Location = new System.Drawing.Point(329, 3);
+            this.minimizeBtn.Name = "minimizeBtn";
+            this.minimizeBtn.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.minimizeBtn.Size = new System.Drawing.Size(16, 16);
+            this.minimizeBtn.TabIndex = 2;
+            this.minimizeBtn.Text = "x";
+            this.minimizeBtn.Click += new System.EventHandler(this.minimizeBtn_Click);
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Impact", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.guna2HtmlLabel1.ForeColor = System.Drawing.Color.White;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(34, 19);
+            this.guna2HtmlLabel1.TabIndex = 3;
+            this.guna2HtmlLabel1.Text = "Bmp3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(115)))), ((int)(((byte)(167)))));
+            this.ClientSize = new System.Drawing.Size(364, 237);
+            this.Controls.Add(this.openMusic);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.topPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.topPanel.ResumeLayout(false);
+            this.topPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel topPanel;
+        private Guna.UI2.WinForms.Guna2CircleButton closeBtn;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private Guna.UI2.WinForms.Guna2Button openMusic;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private Guna.UI2.WinForms.Guna2CircleButton minimizeBtn;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+    }
+}
+
---
+++ bmp32/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bmp32
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        bool Move;
+        int MoveX, MoveY;
+
+        private void topPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            Move = true;
+            MoveX = e.X;
+            MoveY = e.Y;
+        }
+
+        private void topPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Move)
+            {
+                this.SetDesktopLocation(MousePosition.X-MoveX, MousePosition.Y-MoveY);
+            }
+           
+        }
+
+        private void openMusic_Click(object sender, EventArgs e)
+        {
+             OpenFileDialog openFileDialog = new OpenFileDialog();
+             openFileDialog.Filter = "Media Files|*.mp3;";
+    
+    if (openFileDialog.ShowDialog() == DialogResult.OK)
+    {
+        axWindowsMediaPlayer1.URL = openFileDialog.FileName;
+    }
+        }
+
+        private void minimizeBtn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void topPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            Move = false;
+        }
+    }
+}
